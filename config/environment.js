69d05d5065d81e9570d129dc2f0a738a350c4e54
Kory Mathis <kory.mathis@gmail.com>
2015-09-03 13:01:17 -0700

    new client id

--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,7 @@ module.exports = function(environment) {
       'default-src': "accounts.google.com",
       'script-src': "'self' apis.google.com",
       'font-src': "'self'",
-      'connect-src': "'self' ws://localhost:7000 localhost:7000 byway.com https://www.googleapis.com/oauth2/v2/userinfo",
+      'connect-src': "'self' ws://localhost:7000 localhost:7000 byway.com https://www.googleapis.com/oauth2/v2/userinfo *.pidevelopment.org",
       'img-src': "'self' *.googleusercontent.com data:",
       'report-uri':"'localhost'",
       'style-src': "'self' 'unsafe-inline' styles.kushpin.com",
@@ -26,8 +26,8 @@ module.exports = function(environment) {
     torii: {
       providers: {
         'google-api': {
-          clientId: '837903891646.apps.googleusercontent.com',
-          domain: 'localhost:4200'
+          clientId: '75436597455-oonvpgir5pnummj7o012i839rh03babt.apps.googleusercontent.com',
+          domain: 'http://petitions.pidevelopment.org'
         }
       }
     },
@@ -48,6 +48,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.torii.providers['google-api'].domain = "http://localhost:4200";
   }
 
   if (environment === 'test') {
