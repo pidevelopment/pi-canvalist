d4f714a9fd8031d865ddd1421c23a406c98e0a7f
Kory Mathis <kory.mathis@gmail.com>
2015-09-16 14:43:49 -0700

    templates

--- app/routes/campaigns.js
+++ app/routes/campaigns.js
@@ -3,7 +3,11 @@ import { request } from 'ic-ajax';
 
 export default Ember.Route.extend({
   model: function() {
-    return request("http://petitions.pidevelopment.org/api/campaigns").then(function(response) {
+    return request("http://petitions.pidevelopment.org/api/campaigns", {
+      beforeSend: function(request) {
+        request.setRequestHeader("Authorization", 'UUID');
+      }
+    }).then(function(response) {
       Ember.Logger.info("GET campaigns", response);
     });
   }
