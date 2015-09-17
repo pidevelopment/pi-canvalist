1532e52cbe3001b3b27a5f3501412e07c6b78f39
Roger Adams <adamsrog04@gmail.com>
2015-09-17 12:20:36 -0700

    auth stuff

--- app/routes/campaigns.js
+++ app/routes/campaigns.js
@@ -3,9 +3,13 @@ import { request } from 'ic-ajax';
 
 export default Ember.Route.extend({
   model: function() {
+  	// return Ember.$.getJSON("http://petitions.pidevelopment.org/api/campaigns");
+  	var token = this.get('session.secure.token');
+		console.log('token:', token);
+
     return request("http://petitions.pidevelopment.org/api/campaigns", {
       beforeSend: function(request) {
-        request.setRequestHeader("Authorization", 'UUID');
+        request.setRequestHeader("Authorization", 'Bearer ' + token);
       }
     }).then(function(response) {
       Ember.Logger.info("GET campaigns", response);
