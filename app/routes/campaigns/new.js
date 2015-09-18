39b30d09b5276f4dd163c0987c3e84e51227fe8a
Kory Mathis <kory.mathis@gmail.com>
2015-09-18 10:21:00 -0700

    changes to campaign route

--- app/routes/campaigns/new.js
+++ app/routes/campaigns/new.js
@@ -6,20 +6,21 @@ export default Ember.Route.extend({
     create: function() {
     	// create payload for POST
       var campaign = {
-      	owner: this.get('session.secure.token'),
+      	// owner: this.get('session.secure.user.id'),
       	title: this.controllerFor('campaigns.new').get('title'),
       	description: this.controllerFor('campaigns.new').get('description'),
-      	start: Date.now(),
-      	end: Date.now() + 1,
+      	// start: Date.now(),
+      	// end: Date.now() + 1,
       	scope: 'PI'
       };
+      var token = this.get('session.secure.token');
       console.log(campaign)
       return Ember.$.ajax({
       	url: "http://petitions.pidevelopment.org/api/campaigns",
       	type: 'POST',
       	data: campaign,
       	beforeSend: function(request) {
-      		request.setRequestHeader("Authorization", "Bearer " + campaign.owner);
+      		request.setRequestHeader("Authorization", "Bearer " + token);
       	}
       }).then(function(response) {
       	Ember.Logger.info('......', response);
