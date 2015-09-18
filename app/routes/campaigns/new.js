a4b74be4ec7ceb8fa9d9a85127604b5fbe6c933c
Roger Adams <adamsrog04@gmail.com>
2015-09-17 23:38:19 -0700

    fix new campaign stuff

--- app/routes/campaigns/new.js
+++ app/routes/campaigns/new.js
@@ -1,16 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  owner: "",
-  title: "Test",
-  description: "",
-  start: Date.now(),
-  end: Date.now() + 1,
-  scope: "PI",
-
-  actions: {
-    create: function(event) {
-      Ember.Logger.info("title", this.get('title'));
+	actions: {
+    create: function() {
+    	// create payload for POST
+      var campaign = {
+      	owner: this.get('session.secure.token'),
+      	title: this.controllerFor('campaigns.new').get('title'),
+      	description: this.controllerFor('campaigns.new').get('description'),
+      	start: Date.now(),
+      	end: Date.now() + 1,
+      	scope: 'PI'
+      };
+      console.log('...', campaign);
     }
   }
 });
