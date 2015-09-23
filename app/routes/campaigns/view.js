cfe39e2c26f0aeeebf56ad702f57c9df6469563b
Roger Adams <adamsrog04@gmail.com>
2015-09-22 19:45:38 -0700

    clean up model hook for routes/campaign/view

--- app/routes/campaigns/view.js
+++ app/routes/campaigns/view.js
@@ -2,20 +2,8 @@ import Ember from 'ember';
 import { request } from 'ic-ajax';
 
 export default Ember.Route.extend({
-  model: function(model) {
-  	var token = this.get('session.content.secure.token');
-    return request("http://petitions.pidevelopment.org/api/campaigns/" + model.id, {
-      beforeSend: function(request) {
-        Ember.Logger.info("request", request);
-        request.setRequestHeader("Authorization", 'Bearer ' + token);
-      }
-    }).then(function(response) {
-			Ember.Logger.info("GET single campaign", response);
-      if(response.data) {
-        Ember.Logger.info("single campaign response data", response.data);
-        return response.data[0];
-      }
-		});;
+  model: function(params) {
+    return this.store.findRecord('campaign', params.id);
   },
 
   actions: {
