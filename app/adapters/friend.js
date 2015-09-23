2178ca69bea51b51fff7c1293200ee24fab7c99b
Roger Adams <adamsrog04@gmail.com>
2015-09-22 18:24:30 -0700

    campaigns uses ember-data, general cleanup

--- app/adapters/friend.js
+++ app/adapters/friend.js
@@ -2,7 +2,6 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   pathForType: function(modelName) {
-    Ember.Logger.info("setting path for type?");
     return "userajax";
   }
 });
